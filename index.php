8042620b30e03d8c259aa88e6117bdd6172d5e2a
sofiacottone <cottonesofia@outlook.it>
2024-05-02 15:55:44 +0200

    feat: print all elements of the hotels array

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php
+
+$hotels = [
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap css  -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <title>PHP Hotel</title>
+</head>
+
+<body>
+    <ul>
+        <?php foreach ($hotels as $hotel) { ?>
+            <li>
+                <div>Name: <?php echo $hotel['name']; ?></div>
+                <div>Description: <?php echo $hotel['description']; ?></div>
+                <div>Parking: <?php echo $hotel['parking'] ? 'Yes' : 'No'; ?></div>
+                <div>Vote: <?php echo $hotel['vote']; ?></div>
+                <div>Distance to center: <?php echo $hotel['distance_to_center'] . ' km'; ?></div>
+
+            </li>
+        <?php } ?>
+    </ul>
+</body>
+
+</html>
